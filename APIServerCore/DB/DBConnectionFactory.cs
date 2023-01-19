04f7abda6063570ce92d7dff930b588318656850
eqeq1090 <eqeq1090@gmail.com>
2023-01-19 17:56:40 +0900

    DB Connection Factory

--- APIServerCore/DB/DBConnectionFactory.cs
+++ APIServerCore/DB/DBConnectionFactory.cs
@@ -1,6 +1,24 @@
-﻿namespace APIServerCore.DB
+﻿using APIServerCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIServerCore.DB
 {
-    public class DBConnectionFactory
+    public class DBConnectionFactory : DbContext
     {
+        protected readonly IConfiguration Configuration;
+
+        public DBConnectionFactory(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to mysql with connection string from app settings
+            var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+            options.UseMySQL(connectionString!);
+        }
+
+        public DbSet<Test> Tests { get; set; }
     }
 }
